52f8cc018bb3631ebaa8285dd7cb9bce1deccc7f
Hervé BERNARD <71264080+hbernard80@users.noreply.github.com>
2025-11-10 09:48:32 +0100

    fix: corrige le chargement de PHP-DI

--- config/container.php
+++ config/container.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use DI\ContainerBuilder;
 
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
 $containerBuilder = new ContainerBuilder();
 $containerBuilder->addDefinitions(require __DIR__ . '/dependencies.php');
 
